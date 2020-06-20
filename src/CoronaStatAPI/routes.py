c3fc82eb8c28f48ca171ce50b25371c6fb12a6ae
madukm <kawakamimadu@gmail.com>
2020-06-20 20:12:06 -0300

    api WORKINGGGG

--- src/CoronaStatAPI/routes.py
+++ src/CoronaStatAPI/routes.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 import json
 import dataManage
 
@@ -9,4 +10,41 @@ data1 = dataManage.dataManage()
 @app.route("/api/dados/Brasil")
 def get_brasil():
     return json.dumps(data1.region_cases('Brasil').tolist())
+
+@app.route("/api/mortes/Brasil")
+def get_deaths_brasil():
+    return json.dumps(data1.region_deaths('Brasil').tolist())
+
+@app.route("/api/dados/estado")
+def get_state():
+    state_name = request.args.get('q')
+    start_date = request.args.get('startdate')
+    end_date = request.args.get('enddate')
+    data1.data = data1.data.loc[start_date:end_date]
+    return json.dumps(data1.state_cases(state_name).tolist())
+
+@app.route("/api/mortes/estado")
+def get_deaths_state():
+    state_name = request.args.get('q')
+    start_date = request.args.get('startdate')
+    end_date = request.args.get('enddate')
+    data1.data = data1.data.loc[start_date:end_date]
+    return json.dumps(data1.state_deaths(state_name).tolist())
+
+@app.route("/api/dados/cidade")
+def get_city(city_name):
+    city_name = request.args.get('q')
+    start_date = request.args.get('startdate')
+    end_date = request.args.get('enddate')
+    data1.data = data1.data.loc[start_date:end_date]
+    return json.dumps(data1.city_cases(city_name).tolist())
+
+@app.route("/api/mortes/cidade")
+def get_deaths_city(city_name):
+    city_name = request.args.get('q')
+    start_date = request.args.get('startdate')
+    end_date = request.args.get('enddate')
+    data1.data = data1.data.loc[start_date:end_date]
+    return json.dumps(data1.city_deaths(city_name).tolist())
+
 
